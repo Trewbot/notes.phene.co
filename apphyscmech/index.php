b1ad882f350590c293f0f704a6c8d46d9bdda3aa
Trevor Hoglund <trewbot@phene.co>
2015-11-07 12:52:41 -0600

    Added physics notes

---
+++ apphyscmech/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>notes.phene.co</title>
+	<link rel="stylesheet" href="//phene.co/style.css">
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-67481186-2', 'auto');
+		ga('send', 'pageview');
+	</script>
+</head>
+<body>
+	<div class="content">
+		<div>
+			<b>ap physics c: mechanics &mdash; <a href="//phene.co/trewbot">notes by trevor hoglund</a></b>
+			<hr>
+			<a href="//notes.phene.co/apphyscmech/1ed">1ed.<a>
+		</div>
+		
+		
+		<br><br>
+		
+		<p>
+			<a href="//notes.phene.co">notes</a>
+		</p>
+	</div>
+</body>
+</html>
