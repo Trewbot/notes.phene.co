185c0ae83ce752c561104a3abb7d13e81049be60
Trevor Hoglund <trewbot@phene.co>
2015-12-22 05:19:38 -0600

    Added lightbox to all notes

--- diffeq/2ed/index.php
+++ diffeq/2ed/index.php
@@ -17,7 +17,8 @@
 		<div>
 			<b><a href="//notes.phene.co/diffeq">differential equations</a> &ndash; <a href="//notes.phene.co/diffeq/2ed">2ed.</a></b>
 			<hr>
-			Page:
+			<script src="//scripts.phene.co/popup/0.1.1.0017"></script><a lightbox onclick="_g.pu.lightbox('api','read.php','pages.*',0);">read</a>,
+			page:
 			<?	$files = array_diff(scandir('.'), array('..','.'));
 				for($i = 2; $i < count($files)+2; $i++) if(count(explode(' ',$files[$i]))==2) $files[$i] = explode(' ',explode('.',$files[$i])[0])[1];
 				sort($files);
