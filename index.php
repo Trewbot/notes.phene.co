fb5c107a6f234c389f395a919e4bee1e6eeb41f7
Trevor J Hoglund <Trevor J Hoglund>
2016-12-18 12:44:27 -0600

    Minor changes

--- index.php
+++ index.php
@@ -14,48 +14,89 @@
 		<div>
 			<b><a href="//notes.phene.co/">notes</a></b>
 			<hr>
-			<b>art:</b><br>
-			<a href="//notes.phene.co/aparthist">ap art history</a> &ndash; <a href="//notes.phene.co/aparthist/1ed">1ed.</a>
-			
-			<br>
-			<br>
-			
-			<b>chemistry:</b><br>
-			<a href="//notes.phene.co/apchem">ap chemistry</a> &ndash; <a href="//notes.phene.co/apchem/1ed">1ed.</a>
-			
-			<br>
-			<br>
-			
-			<b>mathematics:</b><br>
-			<a href="//notes.phene.co/calci">calculus i</a> &ndash; [<a href="//notes.phene.co/calci/3ed">3ed.</a> <a href="//notes.phene.co/calci/4ed">4ed.</a>],
-			<a href="//notes.phene.co/calcii">calculus ii</a> &ndash; [<a href="//notes.phene.co/calcii/2ed">2ed.</a> <a href="//notes.phene.co/calcii/3ed">3ed.</a>],
-			<a href="//notes.phene.co/calciii">calculus iii</a> &ndash; [<a href="//notes.phene.co/calciii/1ed">1ed.</a> <a href="//notes.phene.co/calciii/2ed">2ed.</a>],
-			<a href="//notes.phene.co/diffeq">differential equations</a> &ndash; [<a href="//notes.phene.co/diffeq/1ed">1ed.</a>, <a href="//notes.phene.co/diffeq/2ed">2ed.</a>],
-			<a href="//notes.phene.co/linalg">linear algebra</a> &ndash; <a href="//notes.phene.co/linalg/1ed">1ed.</a>
-			
-			<br>
-			<br>
-			
-			<b>physics:</b><br>
-			<a href="//notes.phene.co/apphyscmech">ap physics c: mechanics</a> &ndash; [<a href="//notes.phene.co/apphyscmech/1ed">1ed.</a> <span>2ed.</span>],
-			<a href="//notes.phene.co/apphyscem">e&amp;m</a> &ndash; [<a href="//notes.phene.co/apphyscem/1ed">1ed.</a> <span>2ed.</span>];
-			
-			<br>
-			<br>
-			
-			<b>social science:</b><br>
-			<a href="//notes.phene.co/apgov">ap government</a> &ndash; <a href="//notes.phene.co/apgov/2ed">2ed.</a>,
-			<a href="//notes.phene.co/appsych">ap psychology</a> &ndash; <a href="//notes.phene.co/appsych/2ed">2ed.</a>
-		
-			<br>
-			<br>
-			
 			<span>gray &rarr; coming soon</span>
+			<br>
+			<br>
+			<br>
+			<div id="art">
+			<a href="#art"><b>art:</b></a><br>
+			<div class="columns">
+				<a href="//notes.phene.co/aparthist">ap art history</a>:
+					<a href="//notes.phene.co/aparthist/1ed">1ed.</a>
+			</div>
+			</div>
+			<br>
+			<br>
+			<div id="chemistry">
+			<a href="#chemistry"><b>chemistry:</b></a><br>
+			<div class="columns">
+				<a href="//notes.phene.co/apchem">ap chemistry</a>:
+					<a href="//notes.phene.co/apchem/1ed">1ed.</a>
+			</div>
+			</div>
+			<br>
+			<br>
+			<div id="math">
+			<a href="#math"><b>mathematics:</b></a><br>
+			<div class="columns">
+				<a href="//notes.phene.co/calci">calculus i</a>:
+					<a href="//notes.phene.co/calci/3ed">3ed.</a>
+					<a href="//notes.phene.co/calci/4ed">4ed.</a>
+					<br>
+				<a href="//notes.phene.co/calcii">calculus ii</a>:
+					<a href="//notes.phene.co/calcii/2ed">2ed.</a>
+					<a href="//notes.phene.co/calcii/3ed">3ed.</a>
+					<br>
+				<a href="//notes.phene.co/calciii">calculus iii</a>:
+					<a href="//notes.phene.co/calciii/1ed">1ed.</a>
+					<a href="//notes.phene.co/calciii/2ed">2ed.</a>
+					<br>
+				<a href="//notes.phene.co/linalg">linear algebra</a>:
+					<a href="//notes.phene.co/linalg/1ed">1ed.</a>
+					<br>
+				<a href="//notes.phene.co/diffeq">diff. eq.</a>:
+					<a href="//notes.phene.co/diffeq/1ed">1ed.</a>
+					<a href="//notes.phene.co/diffeq/2ed">2ed.</a>
+					<br>
+				<span title="coming may 2016">mpe ii</span>:
+					<span title="coming may 2016">1ed.</span>
+			</div>
+			</div>
+			<br>
+			<br>
+			<div id="physics">
+			<a href="#physics"><b>physics:</b></a><br>
+			<div class="columns">
+				<a href="//notes.phene.co/apphyscmech">ap phys. c mech.</a>:
+					<a href="//notes.phene.co/apphyscmech/1ed">1ed.</a>
+					<span title="coming jan 2016">2ed.</span>
+					<br>
+				<a href="//notes.phene.co/apphyscem">ap phys. c e&amp;m</a>:
+					<a href="//notes.phene.co/apphyscem/1ed">1ed.</a>
+					<br>
+				<span title="coming may 2016">calc.-based phys. ii</span>:
+					<span title="coming may 2016">1ed.</span>
+					<br>
+				<span title="coming may 2016">modern physics</span>:
+					<span title="coming may 2016">1ed.</span>
+					<span title="coming jun 2016">2ed.</span>
+			</div>
+			</div>
+			<br>
+			<br>
+			<div id="social">
+			<a href="#social"><b>social science:</b></a><br>
+			<div class="columns">
+				<a href="//notes.phene.co/apgov">ap government</a>:
+					<a href="//notes.phene.co/apgov/2ed">2ed.</a>
+					<br>
+				<a href="//notes.phene.co/appsych">ap psychology</a>:
+					<a href="//notes.phene.co/appsych/2ed">2ed.</a>
+			</div>
+			</div>
+			<br>
+			<br>
 		</div>
-		
-		
-		<br><br>
-		
 		<a href="//phene.co">phene.co</a>
 	</div>
 </body>
---
+++ test.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<?
+		$pgs = [
+			"notes.phene.co"
+		];
+		// $bg = "http://trevorjhoglund.com/bg/notes.jpg";
+		require_once($_SERVER['DOCUMENT_ROOT'].'./assets/head.php');
+	?>
+</head>
+<body>
+	<div class="content">
+		<div>
+			<div id="calci">
+				<a href="#calci"><b>MATH 166 Calculus I</b></a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 1.jpg">&sect;2.2 Limits Including Infinity</a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 1.jpg">&sect;2.3 Bullshit</a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 1.jpg">&sect;2.4 Rates of Change</a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 1.jpg">&sect;3.1 Derivatives</a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 1.jpg">&sect;3.2 Differentiability Exceptions</a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 1.jpg">&sect;3.4 Velocity &amp; Other Rates of Change</a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 2.jpg">&sect;3.5 Derivatives of Trigonometric Functions</a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 2.jpg">&sect;3.6 Chain Rule</a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 2.jpg">&sect;3.7 Implicit Differentiation</a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 2.jpg">&sect;3.8 Derivative of Inverse Trigonometric Functions</a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 2.jpg">&sect;3.9 Something About e</a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 2.jpg">&sect;4.1 Extreme Values of Functions</a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 3.jpg">&sect;4.2 Mean Value Theorem</a><br>
+				<a href="//notes.phene.co/calci/4ed/Page 3.jpg">&sect;4.3 Analytic Justifications</a><br>
+				...
+			</div>
+			<br>
+			<br>
+			<div id="calcii">
+				<a href="#calcii"><b>MATH 167 Calculus II</b></a><br>
+				<a href="//notes.phene.co/calcii/3ed/Page 8.jpg">&sect;7.1 Inverse Functions</a><br>
+				<a href="//notes.phene.co/calcii/3ed/Page 8.jpg">&sect;7.2 Exponential Functions &amp; Their Derivatives</a><br>
+				...<br>
+				<a href="//notes.phene.co/calcii/3ed/Page 9.jpg">&sect;8.2 Trigonometric Integrals</a><br>
+				<a href="//notes.phene.co/calcii/3ed/Page 9.jpg">&sect;8.3 Trigonometric Substitution</a><br>
+				<a href="//notes.phene.co/calcii/3ed/Page 10.jpg">&sect;8.4 Integration of Rational Functions by Partial Functions</a><br>
+				<a href="//notes.phene.co/calcii/3ed/Page 11.jpg">&sect;7.8 Indeterminate Forms... Again</a><br>
+				<a href="//notes.phene.co/calcii/3ed/Page 11.jpg">&sect;8.8 Improper Integrals</a><br>
+				...
+			</div>
+			<br>
+			<br>
+		</div>
+		<a href="//phene.co">phene.co</a>
+	</div>
+</body>
+</html>
